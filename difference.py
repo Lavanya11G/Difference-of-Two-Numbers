017df76ab887b6b3116ecc402da0e1e603867daf
404Mate <cadenstolz05@Gmail.com>
2022-10-01 12:31:00 -0500

    Rework difference.py

--- difference.py
+++ difference.py
@@ -1,8 +1,6 @@
-from sys import argv
-from math import *
-import pylance
-import sql
-
-a = int(input())
-b = int(input())
-print(a-b)
+a = int(input("What is your first number?\n"))
+b = int(input("What is your second number?\n"))
+difference = a - b
+if difference < 0:
+    difference *= -1
+print(f"The difference between {a} and {b} is {difference}")
