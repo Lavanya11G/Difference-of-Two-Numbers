5d7d895e5028dfdeedf2e9df16717fa184690285
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-10-09 12:09:16 +0530

    Format cpp programs with prettier
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- prime_in_a_range.cpp
+++ prime_in_a_range.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int i,n1,n2;
-    cin>>n1>>n2;
-    for(int num=n1;num<n2;num++){
-        for(i=2;i<num;i++){
-            if(num%i==0){
-                break;
-            }
-        }
-        if(i==num){
-        cout<<num<<" is a prime number\t";
-        }
+int main() {
+  int i, n1, n2;
+  cin >> n1 >> n2;
+  for (int num = n1; num < n2; num++) {
+    for (i = 2; i < num; i++) {
+      if (num % i == 0) {
+        break;
+      }
     }
+    if (i == num) {
+      cout << num << " is a prime number\t";
+    }
+  }
 }
--- simple_cal.cpp
+++ simple_cal.cpp
@@ -1,31 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    float n1,n2;
-    cout<<"enter 2 numbers ";
-    cin>>n1>>n2;
+int main() {
+  float n1, n2;
+  cout << "enter 2 numbers ";
+  cin >> n1 >> n2;
 
-    char op;
-    cout<<" enter an operator "<< endl;
-    cin>>op;
+  char op;
+  cout << " enter an operator " << endl;
+  cin >> op;
 
-    switch (op)
-    {
-    case '+':
-        cout<<n1+n2<<endl;
-        break;
-    case '-':
-        cout<<n1-n2<<endl;
-        break;
-    case '*':
-        cout<<n1*n2<<endl;
-        break;
-    case '/':
-        cout<<n1/n2<<endl;
-        break;
-    
-    default:
-        cout<< "its a simple calculator which only performs +,-,*,/";
-        break;
-    }
+  switch (op) {
+  case '+':
+    cout << n1 + n2 << endl;
+    break;
+  case '-':
+    cout << n1 - n2 << endl;
+    break;
+  case '*':
+    cout << n1 * n2 << endl;
+    break;
+  case '/':
+    cout << n1 / n2 << endl;
+    break;
+
+  default:
+    cout << "its a simple calculator which only performs +,-,*,/";
+    break;
+  }
 }
